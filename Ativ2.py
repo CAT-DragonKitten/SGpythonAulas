17ffc691a74940ce2a4293cc3e60de3966a303fc
CAT-DragonKitten <esquiloliam@gmail.com>
2025-09-28 10:12:55 -0300

    Add files via upload
    new kitten things for you guys

---
+++ Ativ2.py
@@ -0,0 +1,8 @@
+QuantidadeFlechasProtag = 15
+quantidadedevidadoinimigo  = 15
+if QuantidadeFlechasProtag > quantidadedevidadoinimigo:
+    print ("next stage...just kidding, you won")
+if QuantidadeFlechasProtag < quantidadedevidadoinimigo:
+    print ("game over noob!")
+if QuantidadeFlechasProtag == quantidadedevidadoinimigo:
+    print ("you lucky winner!")
