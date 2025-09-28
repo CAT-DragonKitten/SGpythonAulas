d05e4072cd53612772fb0af089a18cfe5453d745
CAT-DragonKitten <esquiloliam@gmail.com>
2025-09-28 11:27:02 -0300

    Attack the D point Dragon

---
+++ Ativ4.py
@@ -0,0 +1,14 @@
+attackTheDPoint = False
+AttackTheAPoint = True
+CasIsAnYak9K = True
+if attackTheDPoint == True:
+    print ("never!")
+if AttackTheAPoint == True:
+    print ("positive!")
+    print ("incoming CAS!")
+if CasIsAnYak9K == True:
+    print ("you've got mailed. BOOM!")
+    
+else:
+    print ("im fine... for now")
+    print ("he's coming!  you've got mailed too. BOOM!")
