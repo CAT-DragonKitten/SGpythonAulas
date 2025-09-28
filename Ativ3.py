3b8d65bfde6d894fb2d5f4296b903142ea6727d4
CAT-DragonKitten <esquiloliam@gmail.com>
2025-09-28 10:47:51 -0300

    another kitten thingy

---
+++ Ativ3.py
@@ -0,0 +1,10 @@
+chuva = True
+Nublado = True 
+seco = False
+if chuva == True:
+    print ("vou buscar o guarda chuva...")
+else:
+    if Nublado == True:
+        print ("vou buscar o guarda chuva mesmo assim")
+    else:
+        print ("nao levarei, e começou a chover na metade do caminho")
