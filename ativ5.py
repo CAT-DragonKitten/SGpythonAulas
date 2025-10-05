a86eebbe1a59461330e9b9fa415a84d1a90439e6
CAT-DragonKitten <esquiloliam@gmail.com>
2025-10-05 11:03:13 -0300

    Create ativ5.py
    a piece of you

---
+++ ativ5.py
@@ -0,0 +1,9 @@
+LiamNota1 = float(input("digite sua nota   "))
+LiamNota2 = float(input("digite sua nota   "))
+LiamNota3 = float(input("digite sua nota   "))
+if (LiamNota1 + LiamNota2 + LiamNota3) /3 < 5:
+    print ("faz o L burro")
+elif (LiamNota1 + LiamNota2 + LiamNota3) / 3  < 10:
+    print ("passou raspando, lixo")
+else:
+    print ("eu sou o melhor.")
