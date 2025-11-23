28052fc5dd0cf4dbccb5c44a8d0a31bf09d2ac78
CAT-DragonKitten <esquiloliam@gmail.com>
2025-11-23 10:19:52 -0300

    Madsen light machinegun! aaahhhhhhhhhh

---
+++ Ativextraofahmg.py
@@ -0,0 +1,5 @@
+import random
+listaComNomes = ["Davi Barbosa","Davi Gerson","Diogo Verissimo","Leandro Vieira","Liam de Castro"]
+print ("este sao os alunos da turma: "+ str(listaComNomes))
+indice = random.randint(0 , 4)
+print (listaComNomes[indice])
