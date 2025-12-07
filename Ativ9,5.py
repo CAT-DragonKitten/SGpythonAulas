b311fdb2a208d14c31b19926f9d864e583e107ca
CAT-DragonKitten <esquiloliam@gmail.com>
2025-12-07 11:14:10 -0300

    Create Ativ9,5.py

---
+++ Ativ9,5.py
@@ -0,0 +1,8 @@
+lista = ["astroneer", "stormworks", "trailmakers", "overcooked 2"]
+for i in lista:
+    if i == "astroneer":
+        print ("astroneer e meu jogo favorito")
+    elif i == "overcooked 2":
+        print (" overcooked 2: odeio.")
+    else:
+        print (i + " jogo legal")
