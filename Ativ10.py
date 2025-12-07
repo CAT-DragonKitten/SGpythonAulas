c5a2c2d05910c6e2a671dbebd10393b6a4a82cf7
CAT-DragonKitten <esquiloliam@gmail.com>
2025-12-07 11:13:25 -0300

    american ***** you!

---
+++ Ativ10.py
@@ -0,0 +1,19 @@
+for letra in input("coloque uma palavra: " ):
+    if letra == "L":
+        print ("L, e uma letra do meu nome")
+    elif letra == "l":
+        print ("l, e uma letra do meu nome")
+    elif letra == "I":
+        print ("I, e uma letra do meu nome")
+    elif letra == "i":
+        print ("i, e uma letra do meu nome")
+    elif letra == "A":
+        print ("A, e uma letra do meu nome")
+    elif letra == "a":
+        print ("a, e uma letra do meu nome")
+    elif letra == "M":
+        print ("M, e uma letra do meu nome")
+    elif letra == "m":
+        print ("m, e uma letra do meu nome")
+    else:
+        print(letra + " nao e uma letra do meu nome")
